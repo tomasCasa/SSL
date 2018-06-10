da6574bcd9fcf15cbc72ec91f438857b1038bd38
tomasCasa <tomas.casadoumecq@gmail.com>
2018-06-10 19:03:15 -0300

    Primer commit

---
+++ TP N 1/TP 1 .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//argc cantidad de parametros 
+//argv vector de parametros
+
+
+int main(int argc, char *argv[]) {
+	
+	
+}
