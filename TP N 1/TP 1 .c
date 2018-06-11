3e4521ca2bcf23fb58a8e04f1ed7b5174f1f3f6b
tomasCasa <tomas.casadoumecq@gmail.com>
2018-06-11 17:51:06 -0300

    empezamos una cola para la tabla de tokens

--- TP N 1/TP 1 .c
+++ TP N 1/TP 1 .c
@@ -6,8 +6,52 @@
 //argc cantidad de parametros 
 //argv vector de parametros
 
+typedef struct{
+	char lexema[100];
+	char token[100];
+	int linea;
+	tablaTokens *sgte;
+} tablaTokens;
 
+void agregarTabla(tablaTokens *&frente,tablaTokens *&fin, char[] valLexema, char[] valToken, int valLinea){
+	tablaTokens *q = (tablaTokens *) malloc (sizeof(tablaTokens))
+	q->lexema = valLexema;
+	q->token = valToken;
+	q->linea = valLinea;
+	q->sgte = NULL;
+	if(*frente == NULL)
+		*frente = q;
+	else
+		*fin->sgte = q;
+	*fin = q;
+}
+
+tablaTokens suprimirTabla (tablaTokens *&frente, tablaTokens *&fin){
+	tablaTokens valor;
+	tablaTokens *q = *frente;
+	
+	valor->lexema = q->lexema;
+	valor->token = q->token;
+	valor->linea = q->linea;
+	
+	*frente->q->sgte;
+	if(*frente == NULL)
+		*fin = NULL;
+	
+	free(q);
+	
+	return valor;
+}
 int main(int argc, char *argv[]) {
+
 	
+	if(argc == 2){
+		//mostrar salida por pantalla
+		
+	}
 	
+	if(argc == 3){
+		//guardar en un txt la salida
+		
+	}
 }
